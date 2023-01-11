dbd43191973660691c7311e154dc877fe99fea3d
Paul Oloyede <52790286+paulayo93@users.noreply.github.com>
2023-01-11 03:23:41 +0100

    screen loading done

--- App.js
+++ App.js
@@ -1,43 +1,47 @@
-import { useEffect, useState, useCallback } from 'react';
-import store from './src/redux-store/store';
-import { View } from 'react-native';
-import { useFonts } from 'expo-font';
+import { useEffect, useState, useCallback } from "react";
+import store from "./src/redux-store/store";
+import { View } from "react-native";
+import { useFonts } from "expo-font";
 
-import 'react-native-gesture-handler';
-import { NavigationContainer } from '@react-navigation/native';
+import "react-native-gesture-handler";
+import { NavigationContainer } from "@react-navigation/native";
 
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { SafeAreaProvider } from "react-native-safe-area-context";
 import { persistStore } from "redux-persist";
 import { PersistGate } from "redux-persist/integration/react";
 import { Provider } from "react-redux";
 
-import { Merriweather_900Black } from '@expo-google-fonts/merriweather';
-import { Inter_300Light, Inter_400Regular, Inter_500Medium, Inter_600SemiBold, Inter_700Bold } from '@expo-google-fonts/inter';
+import { Merriweather_900Black } from "@expo-google-fonts/merriweather";
+import {
+  Inter_300Light,
+  Inter_400Regular,
+  Inter_500Medium,
+  Inter_600SemiBold,
+  Inter_700Bold,
+} from "@expo-google-fonts/inter";
 
-import AppNavigator from './src/navigator/app-navigator';
+import AppNavigator from "./src/navigator/app-navigator";
 
-import * as SplashScreen from 'expo-splash-screen';
+import * as SplashScreen from "expo-splash-screen";
 
 SplashScreen.preventAutoHideAsync();
 
-
 let persistor = persistStore(store);
 
-
 export default function App() {
-
   let customFonts = {
     Merriweather_900Black,
     Inter_300Light,
     Inter_400Regular,
     Inter_500Medium,
     Inter_600SemiBold,
-    Inter_700Bold
-  }
+    Inter_700Bold,
+  };
 
   const [fontsLoaded] = useFonts(customFonts);
+ 
 
- const onLayoutRootView = useCallback(async () => {
+  const onLayoutRootView = useCallback(async () => {
     if (fontsLoaded) {
       await SplashScreen.hideAsync();
     }
@@ -46,21 +50,19 @@ export default function App() {
   if (!fontsLoaded) {
     return null;
   }
-  
 
+    return (
 
-
-  return (
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
           <NavigationContainer>
             <SafeAreaProvider>
-              
               <AppNavigator />
-
+              <View onLayout={onLayoutRootView}/>
             </SafeAreaProvider>
           </NavigationContainer>
         </PersistGate>
       </Provider>
-  );
+    );
+ 
 }
